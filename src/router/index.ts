c2e04f11b0f5e1d98dc105f796f2cf4ac79f7d9b
“yosoroQ” <1249508470@qq.com>
2023-01-08 22:13:53 +0800

    0108_night 电子书管理功能开发_使用PageHelper实现后端分页

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import About from '../views/about.vue'
+import AdminEbook from '../views/admin/admin-ebook.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +10,16 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/about',
+      name: 'About',
+      component: About
+    },
+    {
+      path: '/admin/ebook',
+      name: 'AdminEbook',
+      component: AdminEbook
     },
     // {
     //   path: '/about',
---
+++ src/views/about.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>hello，这是关于页面</h1>
+  </div>
+</template>
+
