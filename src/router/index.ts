ef2b6cbff7282e956b679b13f5275cf70ad3f6f8
“yosoroQ” <1249508470@qq.com>
2023-02-13 22:55:03 +0800

    0213晚上 使用树形选择组件选择父节点

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,11 @@
-import { createRouter, createWebHistory} from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import type { RouteRecordRaw } from 'vue-router'
 import Home from '../views/Home.vue'
 import About from '../views/about.vue'
 import AdminEbook from '../views/admin/admin-ebook.vue'
+import AdminDoc from '../views/admin/admin-doc.vue'
 import AdminCategory from '../views/admin/admin-category.vue'
+
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -25,6 +27,11 @@ const routes: Array<RouteRecordRaw> = [
     path: '/admin/category',
     name: 'AdminCategory',
     component: AdminCategory
+  },
+  {
+    path: '/admin/doc',
+    name: 'AdminDoc',
+    component: AdminDoc
   },
 ]
 
--- src/views/admin/admin-doc.vue
+++ src/views/admin/admin-doc.vue
@@ -49,30 +49,29 @@
   </a-layout>
 
   <a-modal
-      title="分类表单"
+      title="文档表单"
       v-model:visible="modalVisible"
       :confirm-loading="modalLoading"
       @ok="handleModalOk"
   >
-    <a-form :model="category" :label-col="{ span: 6 }" :wrapper-col="{ span: 18 }">
+    <a-form :model="doc" :label-col="{ span: 6 }" :wrapper-col="{ span: 18 }">
       <a-form-item label="名称">
-        <a-input v-model:value="category.name" />
+        <a-input v-model:value="doc.name" />
       </a-form-item>
-      <a-form-item label="父分类">
-        <a-select
-            v-model:value="category.parent"
-            ref="select"
+      <a-form-item label="父文档">
+        <a-tree-select
+            v-model:value="doc.parent"
+            style="width: 100%"
+            :dropdown-style="{ maxHeight: '400px', overflow: 'auto' }"
+            :tree-data="treeSelectData"
+            placeholder="请选择父文档"
+            tree-default-expand-all
+            :replaceFields="{title: 'name', key: 'id', value: 'id'}"
         >
-          <a-select-option value="0">
-            无
-          </a-select-option>
-          <a-select-option v-for="c in level1" :key="c.id" :value="c.id" :disabled="category.id === c.id">
-            {{c.name}}
-          </a-select-option>
-        </a-select>
+        </a-tree-select>
       </a-form-item>
       <a-form-item label="顺序">
-        <a-input v-model:value="category.sort" />
+        <a-input v-model:value="doc.sort" />
       </a-form-item>
     </a-form>
   </a-modal>
@@ -85,11 +84,11 @@ import { message } from 'ant-design-vue';
 import {Tool} from "@/util/tool";
 
 export default defineComponent({
-  name: 'AdminCategory',
+  name: 'AdminDoc',
   setup() {
     const param = ref();
     param.value = {};
-    const categorys = ref();
+    const docs = ref();
     const loading = ref(false);
 
     const columns = [
@@ -98,7 +97,7 @@ export default defineComponent({
         dataIndex: 'name'
       },
       {
-        title: '父分类',
+        title: '父文档',
         key: 'parent',
         dataIndex: 'parent'
       },
@@ -114,7 +113,7 @@ export default defineComponent({
     ];
 
     /**
-     * 一级分类树，children属性就是二级分类
+     * 一级文档树，children属性就是二级文档
      * [{
      *   id: "",
      *   name: "",
@@ -124,7 +123,7 @@ export default defineComponent({
      *   }]
      * }]
      */
-    const level1 = ref(); // 一级分类树，children属性就是二级分类
+    const level1 = ref(); // 一级文档树，children属性就是二级文档
 
     /**
      * 数据查询
@@ -133,15 +132,15 @@ export default defineComponent({
       loading.value = true;
       // 如果不清空现有数据，则编辑保存重新加载数据后，再点编辑，则列表显示的还是编辑前的数据
       level1.value = [];
-      axios.get("http://127.0.0.1:8080/category/all").then((response) => {
+      axios.get("http://127.0.0.1:8080/doc/all").then((response) => {
         loading.value = false;
         const data = response.data;
         if (data.success) {
-          categorys.value = data.content;
-          console.log("原始数组：", categorys.value);
+          docs.value = data.content;
+          console.log("原始数组：", docs.value);
 
           level1.value = [];
-          level1.value = Tool.array2Tree(categorys.value, 0);
+          level1.value = Tool.array2Tree(docs.value, 0);
           console.log("树形结构：", level1);
         } else {
           message.error(data.message);
@@ -150,12 +149,15 @@ export default defineComponent({
     };
 
     // -------- 表单 ---------
-    const category = ref({});
+    // 因为树选择组件的属性状态，会随当前编辑的节点而变化，所以单独声明一个响应式变量
+    const treeSelectData = ref();
+    treeSelectData.value = [];
+    const doc = ref({});
     const modalVisible = ref(false);
     const modalLoading = ref(false);
     const handleModalOk = () => {
       modalLoading.value = true;
-      axios.post("http://127.0.0.1:8080/category/save", category.value).then((response) => {
+      axios.post("http://127.0.0.1:8080/doc/save", doc.value).then((response) => {
         modalLoading.value = false;
         const data = response.data; // data = commonResp
         if (data.success) {
@@ -170,11 +172,49 @@ export default defineComponent({
     };
 
     /**
+     * 将某节点及其子孙节点全部置为disabled
+     */
+    const setDisable = (treeSelectData: any, id: any) => {
+      // console.log(treeSelectData, id);
+      // 遍历数组，即遍历某一层节点
+      for (let i = 0; i < treeSelectData.length; i++) {
+        const node = treeSelectData[i];
+        if (node.id === id) {
+          // 如果当前节点就是目标节点
+          console.log("disabled", node);
+          // 将目标节点设置为disabled
+          node.disabled = true;
+
+          // 遍历所有子节点，将所有子节点全部都加上disabled
+          const children = node.children;
+          if (Tool.isNotEmpty(children)) {
+            for (let j = 0; j < children.length; j++) {
+              setDisable(children, children[j].id)
+            }
+          }
+        } else {
+          // 如果当前节点不是目标节点，则到其子节点再找找看。
+          const children = node.children;
+          if (Tool.isNotEmpty(children)) {
+            setDisable(children, id);
+          }
+        }
+      }
+    };
+
+    /**
      * 编辑
      */
     const edit = (record: any) => {
       modalVisible.value = true;
-      category.value = Tool.copy(record);
+      doc.value = Tool.copy(record);
+
+      // 不能选择当前节点及其所有子孙节点，作为父节点，会使树断开
+      treeSelectData.value = Tool.copy(level1.value);
+      setDisable(treeSelectData.value, record.id);
+
+      // 为选择树添加一个"无"
+      treeSelectData.value.unshift({id: 0, name: '无'});
     };
 
     /**
@@ -182,11 +222,17 @@ export default defineComponent({
      */
     const add = () => {
       modalVisible.value = true;
-      category.value = {};
+      doc.value = {};
+
+      treeSelectData.value = Tool.copy(level1.value);
+
+      // 为选择树添加一个"无"
+      treeSelectData.value.unshift({id: 0, name: '无'});
+
     };
 
     const handleDelete = (id: number) => {
-      axios.delete("http://127.0.0.1:8080/category/delete/" + id).then((response) => {
+      axios.delete("http://127.0.0.1:8080/doc/delete/" + id).then((response) => {
         const data = response.data; // data = commonResp
         if (data.success) {
           // 重新加载列表
@@ -201,7 +247,7 @@ export default defineComponent({
 
     return {
       param,
-      // categorys,
+      // docs,
       level1,
       columns,
       loading,
@@ -210,12 +256,14 @@ export default defineComponent({
       edit,
       add,
 
-      category,
+      doc,
       modalVisible,
       modalLoading,
       handleModalOk,
 
-      handleDelete
+      handleDelete,
+
+      treeSelectData
     }
   }
 });
